396dff3190f2ba76b033521e254134983a66eb15
Markus Timtner <markus@timtner.com>
2026-06-19 13:25:27 +0200

    Expand `georgringer/news` compatibility to include versions 13 and 14; update extension version to 2.0.1.

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@ringer.it',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
-            'news' => '11.0.0-12.99.99',
+            'news' => '11.0.0-14.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
